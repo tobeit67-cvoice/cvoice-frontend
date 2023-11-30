0153346e9dd21e1501295cd1d7f9dc2b163be813
pure code <116143995+france8520@users.noreply.github.com>
2023-11-30 23:05:27 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,20 @@ export default function Home() {
       <div style={{width: '100%', height: '100%', position: 'relative', background: 'white'}}>
       <div style={{width: 1512, height: 802, left: 0, top: 113, position: 'absolute', background: '#F8FAFC'}} />
       <div style={{width: 1512, height: 113, left: 0, top: 0, position: 'absolute', background: '#06B6D4', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'}} />
-      <img style={{width: 892.99, height: 686.75, left: 1791.06, top: 920.23, position: 'absolute', transform: 'rotate(-176.97deg)', transformOrigin: '0 0'}} src="https://raw.githubusercontent.com/france8520/IMAGE-OF-C-VOICE-01/blob/main/5c8642e13bcc738cf05fdef0f1e30978.png" />
+      <img
+          style={{
+            width: 892.99,
+            height: 686.75,
+            left: 1000.06,
+            top: 275,
+            position: 'absolute',
+            transform: 'rotate(-360.97deg)',
+            transformOrigin: '0 0'
+          }}
+          src="https://raw.githubusercontent.com/france8520/IMAGE-OF-C-VOICE-01/main/hand_1.png"
+
+        />
+      </div>
       <div style={{width: 1512, height: 67, left: 0, top: 915, position: 'absolute', background: '#ECFCCB'}} />
       <div style={{width: 1442, left: 35, top: 931, position: 'absolute', textAlign: 'center', color: '#0891B2', fontSize: 27, fontFamily: 'Roboto Mono', fontWeight: '400', wordWrap: 'break-word'}}>Copyright © Kernel Knight @ToBeIT’67 The Second</div>
       <div style={{width: 816, left: 117, top: 357, position: 'absolute', textAlign: 'center', color: '#0893B4', fontSize: 40, fontFamily: 'Roboto Mono', fontWeight: '400', wordWrap: 'break-word'}}>แปลงไฟล์เสียงของคุณเป็นข้อความที่อ่านง่าย<br/>แถมได้รับการตรวจสอบและสรุปมาแล้วกันเลย</div>
