82db66119fdd3cbfb562ef1cf3725ef0306fad0e
pure code <thananoon@gmail.com>
2023-12-01 20:10:59 +0700

    F1-frontend-2

---
+++ app/upload.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+       <div style={{width: '100%', height: '100%', position: 'relative', background: 'white'}}>
+    <div style={{width: 1512, height: 802, left: 22, top: 113, position: 'absolute', background: '#F8FAFC'}} />
+    <div style={{width: 1512, height: 113, left: 0, top: 0, position: 'absolute', background: '#06B6D4', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'}} />
+    <img style={{width: 170, height: 170, left: 42, top: -29, position: 'absolute'}} src="https://via.placeholder.com/170x170" />
+    <div style={{width: 242, height: 46, left: 428, top: 34, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 35, fontFamily: 'Roboto Mono', fontWeight: '700', wordWrap: 'break-word'}}>วิธีใช้งาน</div>
+    <div style={{width: 242, height: 46, left: 212, top: 34, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 35, fontFamily: 'Roboto Mono', fontWeight: '700', wordWrap: 'break-word'}}>หน้าหลัก</div>
+    <div style={{width: 242, height: 46, left: 657, top: 34, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 35, fontFamily: 'Roboto Mono', fontWeight: '700', wordWrap: 'break-word'}}>อัพโหลดไฟล์</div>
+    <div style={{width: 1512, height: 67, left: 0, top: 915, position: 'absolute', background: '#ECFCCB'}} />
+    <div style={{width: 1442, left: 35, top: 931, position: 'absolute', textAlign: 'center', color: '#0891B2', fontSize: 27, fontFamily: 'Roboto Mono', fontWeight: '400', wordWrap: 'break-word'}}>Copyright © Kernel Knight @ToBeIT’67 The Second</div>
+    <div style={{width: 832, height: 357, left: 328, top: 411, position: 'absolute', background: '#FFEDD5', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 100}} />
+    <div style={{width: 552, height: 59, left: 468, top: 542, position: 'absolute', textAlign: 'center', color: '#075985', fontSize: 64, fontFamily: 'Roboto Mono', fontStyle: 'italic', fontWeight: '700', wordWrap: 'break-word'}}>อัปโหลดไฟล์เสียงที่นี่</div>
+    <div style={{width: 1106, left: 203, top: 214, position: 'absolute', textAlign: 'center', color: '#0891B2', fontSize: 48, fontFamily: 'Roboto Mono', fontWeight: '700', wordWrap: 'break-word'}}>แปลงเสียงเป็นข้อความง่ายๆได้ด้วยตัวคุณ !</div>
+    </div>
+      </main>
+    );
+  }
