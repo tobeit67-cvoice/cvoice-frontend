92f48f0d8071b52416e67be6e2d64c5f876115bc
nicenathapong <84279400+nicenathapong@users.noreply.github.com>
2024-05-29 06:23:22 +0000

    feat: blank footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-[#ECFCCB] text-[#0891B2] text-center px-6 py-3">
-      Copyright © Kernel Knight @ToBeIT’67 The Second
+      {/* Copyright © Kernel Knight @ToBeIT’67 The Second */}
     </footer>
   );
 }
